7d419a7252578eef58144cb68ead066b1d2e8826
TADAS-PC\taaas <tadas.mockvicius@gmail.com>
2019-01-18 09:43:28 +0200

    pirma pamoka

---
+++ snake.js
@@ -0,0 +1,14 @@
+var ctx = document.getElementById('ctx').getContext('2d');
+var WIDTH =500;
+var HEIGHT =500;
+ctx.font = "20px Calibri";
+
+var snakeBody = {
+    width:20,
+    height:20
+};
+
+var food = {
+    width: 20,
+    height: 20
+}
