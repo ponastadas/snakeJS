9487a54301ea89cda2c371ad9e80252fff435e81
TADAS-PC\taaas <tadas.mockvicius@gmail.com>
2019-02-01 10:15:01 +0200

    trecia pamoka

--- 4th lesson/gameantak.js
+++ 4th lesson/gameantak.js
@@ -10,20 +10,50 @@
     var WIDTH = 500;
     var HEIGHT = 500;
     //snake body
-    var snakeList, foodList
+    var snakeList, foodList, /* new var:*/direction;
     ctx.font = "20px Calibri";
+
+
     //snake
     var snakeBody = {
         width:20,
         height:20,
         color:'green'
     };
+
     //food
     var food = {
         width:20,
         height:20,
         color:'orange'
     };
+
+    //add new event
+    document.onkeydown = function(event) {
+        //0 - Left
+        //1 - Up
+        //2 - Right
+        //3 - Down
+        if (event.keyCode == 37) {
+            direction = 0;
+            console.log("0");
+        }
+        else if (event.keyCode == 38) {
+            direction = 1;
+            console.log("1");
+        }
+        else if (event.keyCode == 39) {
+            direction = 2;
+            console.log("2");
+        }
+        else if (event.keyCode == 40) {
+            direction = 3;
+            console.log("3");
+        }
+    }
+
+
+
     drawSnake = function(sb,i) {
         //sb = snakeBody; i = index
         ctx.save();
@@ -33,23 +63,36 @@
             ctx.fillStyle = snakeBody.color;
         ctx.fillRect(sb.x, sb.y, snakeBody.width, snakeBody.height);
         ctx.restore();
+
     }
+
     drawFood = function (f,i) {
         //f = food; i = index
         ctx.save();
         ctx.fillStyle = food.color;
         ctx.fillRect=(f.x, f.y, food.width, food.height);
         ctx.restore();
+
     }
+
+
     startGame = function () {
         snakeList = [
             {x: 220, y: 200},
             {x: 210, y: 200},
             {x: 200, y: 200}
         ];
+
         foodList = [];
+        //new var
+        direction= 99;
         snakeList.forEach(drawSnake);
     }
+    startGame();
+
+    //
+
+
     startGame();
 
 </script>
